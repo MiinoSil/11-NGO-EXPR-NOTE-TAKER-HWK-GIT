a728138b502681debd730615f5524204d5a46a32
Andrew Ngo <andvngo@gmail.com>
2022-03-12 17:07:08 -0800

    fixed database requirement

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const database = require("./database/database")
+const database = require("./db/db")
 
 // // Establish routes to HTML
 // const apiRoutes = require("./routes/apiRoutes");
